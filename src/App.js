42fb8de9dcb51534965878a8ce41e32432fc5635
Luuk4mst3l <l.e.amstel@gmail.com>
2022-06-08 16:05:03 +0200

    y

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import './App.css';
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Home from './Home';
+import {CheckpointSelect, CheckpointCreate} from './Checkpoint';
+import {Create, CreateWalk, CreateNow} from './Create';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 let d;
 
@@ -22,16 +25,51 @@ function App() {
   }, [])
   console.log(d);
   return (
-    <div className="App">
+    <><div className="App">
       <h1>Connect React JS with Laravel</h1>
-      {
-        routes.map((routes, i)=>{
-          return (
-            <h2 key={i}>{routes.stad}</h2>
-          )
-        })
-      }
-    </div>
+      {routes.map((routes, i) => {
+        return (
+          <h2 key={i}>{routes.stad}</h2>
+        );
+      })}
+    </div><Router>
+        <section className="App">
+          {/* <Navbar/> */}
+          <section className="content">
+            <Switch>
+              <Route exact path="/">
+                {/* <Navbar/> */}
+                <Home />
+              </Route>
+
+              <Route exact path="/create">
+                <Create />
+              </Route>
+
+              <Route exact path="/create/now">
+                <CreateNow />
+              </Route>
+
+              <Route exact path="/create/walk">
+                <CreateWalk />
+              </Route>
+
+              <Route exact path="/create/checkpoint">
+                <CheckpointSelect />
+              </Route>
+
+              <Route path="/create/checkpoint/">
+                <CheckpointCreate />
+              </Route>
+
+              {/* <Route path="/play">
+      <Play/>
+    </Route> */}
+
+            </Switch>
+          </section>
+        </section>
+      </Router></>
   );
 }
 
