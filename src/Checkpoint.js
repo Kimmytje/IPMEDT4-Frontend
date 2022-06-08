c10972c3e8584cf7684c4b9d9c0c0a1503157e37
Kimmyli <kc.szul@gmail.com>
2022-06-08 13:27:24 +0200

    Update Checkpoint.js

--- src/Checkpoint.js
+++ src/Checkpoint.js
@@ -3,9 +3,9 @@ import CardList from './Cards';
 
 const CheckpointSelect = () => {
     const [cards, setCards] = useState([
-        {title: 'Puzzel', examples: ['Een raadsel', 'Multiplechoise'], imagePath: '/Images/Puzzel.png', link: '/create/checkpoint/puzzel'},
+        // {title: 'Puzzel', examples: ['Een raadsel', 'Multiplechoise'], imagePath: '/Images/Puzzel.png', link: '/create/checkpoint/puzzel'},
         {title: 'Comment', examples: ['Opmerking', 'Verhaal'], imagePath: '/Images/Comment.png', link: '/create/checkpoint/comment'},
-        {title: 'Picture', examples: ['Foto met beschrijving', 'Zoek locatie'], imagePath: '/Images/Picture.png', link: '/create/checkpoint/picture'},
+        // {title: 'Picture', examples: ['Foto met beschrijving', 'Zoek locatie'], imagePath: '/Images/Picture.png', link: '/create/checkpoint/picture'},
         {title: 'Action', examples: ['Wacht * seconden', 'Doe x jumpingjacks'], imagePath: '/Images/Action.png', link: '/create/checkpoint/action'},
     ]);
 
@@ -18,12 +18,6 @@ const CheckpointCreate = () => {
     return (  
         <section className='create-checkpoint'>
             <form >
-                <label>Checkpoint naam:</label>
-                <input
-                    type="text" 
-                    required
-                />
-
                 <label>Vraag:</label>
                 <textarea
                     required
