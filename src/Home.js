fd4e69ba3274c263c37a5b662842052a04e1cea6
DennisHSLeiden <dennisleyting@gmail.com>
2022-06-17 17:38:59 +0200

    Test data to submit test
    heb een form gemaakt met submit knop die de test data van gebruiker in de sql database zet

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,16 @@
 import { useState } from 'react';
 import ButtonList from './Buttons';
 
+
+let voornaam = 'Iemand'
+let achternaam = 'Anders'
+let email = 'Iemand.j@hsleiden.nl'
+let woonplaats = 'Niemandsland'
+
+
+
+
+
 const Home = () => {
     const [buttons, setButtons] = useState([
         {title: 'Maak route', link: '/name'},
@@ -8,9 +18,18 @@ const Home = () => {
     ]);
 
     return ( 
+        <>
         <article className="home">
             <ButtonList buttons={buttons} title="Welcome"/>
         </article>
+        <form action="http://127.0.0.1:8000/api/testaddgebruiker" method="POST" name="bank_form">
+            <input type="text" name="voornaam" value={voornaam} />
+            <input type="text" name="achternaam" value={achternaam} />
+            <input type="text" name="email" value={email} />
+            <input type="text" name="woonplaats" value={woonplaats} />
+            <button type = 'submit'>Click to submit</button>
+        </form>
+        </>
      );
 }
  
