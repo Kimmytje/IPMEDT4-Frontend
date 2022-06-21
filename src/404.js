e75603e0f05fd3c4b03b41161f9646722bce77e9
Kimmyli <kc.szul@gmail.com>
2022-06-21 16:16:39 +0200

    404 page!

--- src/404.js
+++ src/404.js
@@ -3,15 +3,23 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return ( 
         <article className="notfound">
-            <section>
-                <h2>Weird...</h2>
-                <p>It seems like we dont have this page available</p>
-                <Link to="/">
-                    <section className="btn-primary">
-                        <p>Go to out homepage</p>
+                <h1>404</h1>
+
+                <section className="spotlight-wrapper">
+                    <section className="spotlight-container">
+                        <section className="spotlight"></section>
                     </section>
-                </Link>
-            </section>
+                </section>
+
+                <section className="notfound-info">
+                    <h2>We can't find that page</h2>
+                    <p>Weird, we swaer this page used to be here. Unless ofcourse you just typed in some random letters to see where it would take you.... Naaahhh you would never, right?</p>
+                    <Link to="/">
+                        <section className="btn-primary">
+                            <p>Home</p>
+                        </section>
+                    </Link>
+                </section>
         </article>
      );
 }
